b559ce4db7e90d82e946742312172548a613bbab
Brunogonzalez00 <a2021113701@estudiantes.upsa.edu.bo>
2024-11-25 05:06:17 -0400

    .

--- src/pages/Users.tsx
+++ src/pages/Users.tsx
@@ -90,7 +90,7 @@ const Users: React.FC = () => {
       if (newPassword) {
         updateData.password = newPassword;
       }
-      await axios.put(`${import.meta.env.VITE_API_URL}/update-user/:${editingUser.id}`, updateData, { withCredentials: true });
+      await axios.put(`${import.meta.env.VITE_API_URL}/users/update-user/${editingUser.id}`, updateData, { withCredentials: true });
       setEditingUser(null);
       setNewPassword('');
       fetchUsers();
@@ -104,7 +104,7 @@ const Users: React.FC = () => {
   const handleDeleteUser = async (id: number) => {
     if (window.confirm('¿Estás seguro de que quieres eliminar este usuario?')) {
       try {
-        await axios.delete(`${import.meta.env.VITE_API_URL}/delete-user/:${id}`, { withCredentials: true });
+        await axios.delete(`${import.meta.env.VITE_API_URL}/users/delete-user/${id}`, { withCredentials: true });
         fetchUsers();
         setSuccessMessage('Usuario eliminado exitosamente');
       } catch (error) {
